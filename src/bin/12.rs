5f68422324f2170b424e5bbafc30acf47d68ad45
Joshua Smallwood <31932412+smallwoj@users.noreply.github.com>
2023-12-14 01:10:16 -0600

    finish day 12

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -70,7 +70,7 @@ pub fn part_two(input: &str) -> Option<u64> {
         };
         let mut sequence = vec![];
         for num_str in sequence_str.split(',') {
-            sequence.push(num_str.parse::<u32>().unwrap_or(0));
+            sequence.push(num_str.parse::<usize>().unwrap_or(0));
         }
         let mut new_string = "".to_owned();
         let mut new_sequence = vec![];
@@ -81,82 +81,46 @@ pub fn part_two(input: &str) -> Option<u64> {
             new_string += springs;
             new_sequence.extend(sequence.clone());
         }
+        new_string += ".";
         conditions.push((new_string, new_sequence));
     }
-    println!("gotten here at least {:?}", conditions);
 
-    fn generate_arrangements(original: String, sequence: &Vec<u32>) -> Vec<String> {
-        let mut arrangements = vec![String::from("")];
-        let mut subsequences = vec![];
-        let mut curr_sequence = vec![];
-        for s in sequence {
-            curr_sequence.push(*s);
-            subsequences.push(curr_sequence.clone());
+    fn count_possible_arrangements(
+        springs: &[char],
+        counts: &[usize],
+        cache: &mut [Vec<Option<u64>>],
+    ) -> u64 {
+        if counts.is_empty() {
+            return if springs.contains(&'#') { 0 } else { 1 };
         }
-        for c in original.chars() {
-            let mut new_arrangements = vec![];
-            match c {
-                '.' => {
-                    for arr in arrangements {
-                        new_arrangements.push(format!("{}{}", arr, c));
-                    }
-                }
-                '#' => {
-                    for arr in arrangements {
-                        new_arrangements.push(format!("{}{}", arr, c));
-                    }
-                }
-                '?' => {
-                    for arr in arrangements {
-                        new_arrangements.push(format!("{}{}", arr, '.'));
-                        new_arrangements.push(format!("{}{}", arr, '#'));
-                    }
-                }
-                _ => panic!("invalid char"),
-            }
-            new_arrangements = new_arrangements
-                .iter()
-                .filter(|arrangement| {
-                    let trimmed_arrangement = arrangement.trim_matches('.');
-                    let mut arrangement_sequence = trimmed_arrangement
-                        .split('.')
-                        .filter(|s| !s.is_empty())
-                        .map(|s| s.len() as u32)
-                        .collect::<Vec<u32>>();
-                    if arrangement_sequence[..].len() == 1 || arrangement_sequence[..].is_empty() {
-                        return true;
-                    }
-                    if let Some(valid_sequence) = subsequences
-                        .iter()
-                        .find(|ss| ss.len() == arrangement_sequence.len() - 1)
-                    {
-                        arrangement_sequence.pop();
-                        arrangement_sequence == *valid_sequence
-                    } else {
-                        false
-                    }
-                })
-                .cloned()
-                .collect::<Vec<String>>();
-            arrangements = new_arrangements;
+        if springs.len() < counts.iter().sum::<usize>() + counts.len() {
+            return 0;
         }
+        if let Some(cached) = cache[counts.len() - 1][springs.len() - 1] {
+            return cached;
+        }
+        let mut arrangements = 0;
+        if springs[0] != '#' {
+            arrangements += count_possible_arrangements(&springs[1..], counts, cache);
+        }
+        let next_group_size = counts[0];
+        if !springs[..next_group_size].contains(&'.') && springs[next_group_size] != '#' {
+            arrangements +=
+                count_possible_arrangements(&springs[next_group_size + 1..], &counts[1..], cache);
+        }
+        cache[counts.len() - 1][springs.len() - 1] = Some(arrangements);
         arrangements
     }
 
     let mut num_arrangements = 0;
 
     for (springs, sequence) in conditions {
-        for arrangement in generate_arrangements(springs, &sequence) {
-            let trimmed_arrangement = arrangement.trim_matches('.');
-            let arrangement_sequence = trimmed_arrangement
-                .split('.')
-                .filter(|s| !s.is_empty())
-                .map(|s| s.len() as u32)
-                .collect::<Vec<u32>>();
-            if arrangement_sequence == sequence {
-                num_arrangements += 1;
-            }
-        }
+        let mut cache = vec![vec![None; springs.len()]; sequence.len()];
+        num_arrangements += count_possible_arrangements(
+            &springs.chars().collect::<Vec<_>>()[..],
+            &sequence[..],
+            &mut cache,
+        );
     }
 
     Some(num_arrangements)
@@ -177,7 +141,17 @@ mod tests {
         //        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
         //        assert_eq!(result, Some(525152));
 
-        let result = part_two(".??..??...?##. 1,1,3");
+        let mut result = part_two("???.### 1,1,3");
+        assert_eq!(result, Some(1));
+        result = part_two(".??..??...?##. 1,1,3");
         assert_eq!(result, Some(16384));
+        result = part_two("?#?#?#?#?#?#?#? 1,3,1,6");
+        assert_eq!(result, Some(1));
+        result = part_two("????.#...#... 4,1,1");
+        assert_eq!(result, Some(16));
+        result = part_two("????.######..#####. 1,6,5");
+        assert_eq!(result, Some(2500));
+        result = part_two("?###???????? 3,2,1");
+        assert_eq!(result, Some(506250));
     }
 }
